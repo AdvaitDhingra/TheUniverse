8e954c572490679cbb367c8a1ef697116d5105fb
AdvaitDhingra <advait.dhingra1006@gmail.com>
2021-04-25 17:34:54 +0200

    Started work on file reading

--- core/Main.cxx
+++ core/Main.cxx
@@ -1,7 +1,15 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 #include "src/Heat.cxx"
 
 int main(Double_t startingTemp, Double_t acceleration) {
+
+  fstream godFile;
+
+  godFile.open("../god.txt", ios::in);
+  
+  
 
   Heat(1200, 120);
 
